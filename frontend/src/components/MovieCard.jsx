8819b2076257972005d94a8da2f541e726f43899
mdodson22 <melissadodson22@gmail.com>
2025-08-27 23:18:45 -0700

    bug fixes, removed whitespace

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -7,9 +7,7 @@ function MovieCard({movie}) { // movie is an object, information about movie
         alert("clicked")
     }
     
-    return 
-
-    <div className="movie-card">
+    return <div className="movie-card">
         <div className="movie-poster">
             <img src={movie.url} alt={movie.title} />
             <div className="movie-overlay">
