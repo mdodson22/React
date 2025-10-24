82f6eab08d3bb173ac8a4511e3d31f16509e31d1
mdodson22 <melissadodson22@gmail.com>
2025-10-23 18:19:56 -0700

    search functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import MovieCard from "../components/MovieCard"
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("")
+
+
     const movies = [
         {id: 1, title: "The Princess Bride", release_date: "1987"},
         {id: 2, title: "Dead Poets Society", release_date: "1989"},
@@ -9,18 +13,29 @@ function Home() {
     ]
 
     const handleSearch = () => {
-
+        e.preventDefault()
+        alert(searchQuery)
     }
 
     return (
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search for movies ..." className="search-input"/>
+                <input 
+                type="text" 
+                placeholder="Search for movies ..." 
+                className="search-input"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
             </form>
             <button type="submit" className="search-button">Search</button>
             <div className="movies-grid-structure">
-            {movies.map((movie) => (<MovieCard movie={movie} key={movie.id} />
-            ))}
+            {movies.map(
+                (movie) => 
+                movie.title.toLowerCase().startsWith(searchQuery) && 
+                    (<MovieCard movie={movie} key={movie.id} />
+                    )
+            )}
         </div>
     </div>
     );
